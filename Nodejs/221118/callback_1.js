aa15f5cb06d8df605324a6326d4497e6ecc2dadc
nietzche15 <yeongeun93@gmail.com>
2022-11-19 00:38:07 +0900

    feat:commit

--- Nodejs/221118/callback_1.js
+++ Nodejs/221118/callback_1.js
@@ -1,4 +1,29 @@
-function fruits( fav1, fav2, nt ){
-    console.log( "1st place : " + fav1);
-    nt( fav2 );
+const prompt = require('prompt-sync')();
+const name = prompt("What is your least favorite fruit?");
+
+function fruits( fruit1, fruit2, nt ){
+    setTimeout( function(){
+        console.log( "1st place : " + fruit1 );
+        nt( fruit2 );
+    },1000)
 }
+function secondFruit( secFruit, nt ){
+    setTimeout( function(){
+        console.log( "2nd place : " + secFruit );
+        nt( `${name}` );
+    },1000)
+}
+function hateFruit( hateFruit, nt ){
+    setTimeout( function(){
+        nt( "least favorite fruit : " + hateFruit );
+    },1000)
+    
+}
+
+fruits('WATERMELON', 'Shine Muscat', function( f1 ){
+    secondFruit( f1, function( f2 ){
+        hateFruit( f2, function( f3 ){
+            console.log( f3 );
+        });
+    });
+});
