5de06a87f0c2e6d90c10799c49c02c60ae905267
nietzche15 <yeongeun93@gmail.com>
2022-11-20 14:55:41 +0900

    feat:commit

--- Nodejs/221120/http_1.js
+++ Nodejs/221120/http_1.js
@@ -1,1 +1,12 @@
+const { setServers } = require("dns");
 const http = require("http");
+const fs = require("fs").promises;
+
+const server = http.createServer( ( req, res ) =>{
+    fs.readFile('./string_int_.html')
+    .then( (data)=> { res.end( data.toString());})
+});
+
+server.listen( 8080, function(){
+    console.log("Port 8080 connected");
+})
