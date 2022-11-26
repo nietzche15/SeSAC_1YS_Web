675ca54dccd6746801bfb7f5185252c65f455c36
nietzche15 <yeongeun93@gmail.com>
2022-11-27 01:34:42 +0900

    feat;commit

--- Nodejs/221125/form_static.js
+++ Nodejs/221125/form_static.js
@@ -6,24 +6,23 @@ app.set('view engine', 'ejs');
 app.use( express.urlencoded({extended:true}));
 app.use( express.json());
 
-app.use( '/views', express.static(__dirname+'views'));
+app.use( '/views', express.static(__dirname+'/views'));
 app.use( express.static('static'));
 
+
+
 app.get('/form', (req,res)=>{
-    res.render('app_static_G');    
+    res.render('form_static');    
 })
 
 app.get('/getForm',(req,res)=>{
     console.log(res.query);
-    res.send('getForm success');
+    res.send('getForm SUCCESS !');
 })
 
 app.post('/postForm', (req,res)=>{
-    res.render('app_static_P');    
-})
-app.post('/postForm',(req,res)=>{
-    console.log(req.body);
-    res.end('postForm success')
+    console.log( req.body );
+    res.render('form_getPost', { data : req.body, title : 'getForm Data'});    
 })
 
 app.listen( port, ()=>{
