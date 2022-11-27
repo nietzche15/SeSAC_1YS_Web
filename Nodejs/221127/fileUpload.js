3e62345e858b5ea8fde1489637039930bb58d2f3
nietzche15 <yeongeun93@gmail.com>
2022-11-28 01:32:03 +0900

    feat:commit

--- Nodejs/221127/fileUpload.js
+++ Nodejs/221127/fileUpload.js
@@ -14,7 +14,7 @@ const upload = multer({
             done( null, 'uploads/');
         },
         filename( req, file, done ){
-            console.log( 'filename : ', req.body );
+            console.log( 'filename : ', req.body ); //file input 이전까지만 처리
 
             const ext = path.extname( file.originalname );
             const filename = Math.random() + ext;
@@ -26,14 +26,31 @@ const upload = multer({
 
 
 app.use( '/views', express.static('/views'));
-app.use( '/uploads', express.static('/uploads'));
+app.use( '/uploads', express.static( __dirname+'/uploads'));
 
 app.get('/', (req,res)=>{
     res.render('fileUpload');
 })
 
+app.post('/single_upload', upload.single('userfile'), (req,res)=>{
+    console.log( 'req.file : ', req.file);
+    console.log( 'req.body : ', req.body);
+    res.render( 'postFile', { file : req.file, files : null });
+})
+
+app.post('/array_upload', upload.array('userfile'), (req,res)=>{
+    console.log( 'req.files : ', req.files);
+    console.log( 'req.body : ', req.body);
+    res.render('postFile', { file : null, files : req.files });
+})
+
+app.post('/fields_upload', upload.fields([{name:'userfile1'},{name:'userfile2'},{name:'userfile3'}]), (req,res)=>{
+    console.log( 'req.files : ', req.files);
+    console.log( 'req.body : ', req.body);
+    res.render('postFile', { file : null, files : req.files });
+})
 
 
 app.listen( port, ()=>{
-    console.log( port + 'is connected ! ');
+    console.log( port + ' is connected ! ');
 })
