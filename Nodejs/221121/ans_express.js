e50a5a0af371527858a71fb1d34ac003a233144b
nietzche15 <yeongeun93@gmail.com>
2022-11-29 23:57:32 +0900

    feat:commit

--- Nodejs/221121/ans_express.js
+++ Nodejs/221121/ans_express.js
@@ -2,12 +2,12 @@ const express =require('express');
 const app = express();
 const port = 3030;
 
-app.listen( port, ()=>{
-    console.log( port +' port is connected');
-})
-
-app.use(express.static('./'));
+app.use( express.static('./')); //현재 위치 이하의 dir만 set 가능
 
 app.get('/', ( req, res )=>{
-    res.sendFile(__dirname + '/just.html');
+    res.sendFile(__dirname +'/just.html'); 
+})
+
+app.listen( port, ()=>{
+    console.log( port +' port is connected');
 })
