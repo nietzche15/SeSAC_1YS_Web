dbb5556ad6e8e8b6d45a104d16226b65ec7e9cfe
nietzche15 <yeongeun93@gmail.com>
2022-12-05 23:58:53 +0900

    feat:commit

---
+++ Nodejs/221205/model/M.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql'); 
+
+const cnn = mysql.createConnection({
+    host : 'localhost', 
+    user : 'user',
+    password : '#hibiscuS15',
+    database : 'hibiscus',
+});
