c5633ec27bf48180db9ecaedab4fe595972dc438
nietzche15 <yeongeun93@gmail.com>
2023-02-09 16:05:05 +0900

    Feat:230209 socket 기능 완성, ui 수정

--- 230208/server/index.js
+++ 230208/server/index.js
@@ -12,21 +12,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/../src/App/js');
 });
 
-let nickNameList = {};
-let cnt = 0;
+let nickNameList = { admin: 'Server' };
+
+// const username = prompt('닉네임을 입력해주세요.(5자 이하 권장)');
 
 io.on('connection', (socket) => {
   // 닉네임 익명N으로 설정, 익명N-socketID key-value로 저장
-  cnt++;
-  let myNickName = '익명' + cnt;
+  let myNickName = '익명' + Object.keys(nickNameList).length;
   nickNameList[socket.id] = myNickName;
 
   // welcome User
   console.log('Server Socket Connected', socket.id);
   socket.emit('welcome', { msg: 'Server Socket welcome' });
-  io.to(`${socket.id}`).emit('getDM', { msg: `Hello, ${myNickName}` });
-
-  io.emit('nickNameList', nickNameList);
 
   // Realtime User Notice
   io.emit('notice', {
@@ -35,6 +32,8 @@ io.on('connection', (socket) => {
   });
   // User 입장 시 개인 welcome msg
   io.to(`${socket.id}`).emit('getDM', {
+    from_id: 'admin',
+    to_id: socket.id,
     msg: `Hello, ${myNickName}`,
   });
 
@@ -48,9 +47,6 @@ io.on('connection', (socket) => {
 
   // DM 수신 후 전송 (보낸 user, 받는 user both)
   socket.on('sendDM', (data) => {
-    console.log(data);
-    console.log(nickNameList[data.from_id]);
-
     io.to(`${data.to_id}`).to(`${data.from_id}`).emit('getDM', {
       from_id: data.from_id,
       to_id: data.to_id,
@@ -59,8 +55,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('Server Socket disconnected');
-    delete nickNameList[socket.id];
+    console.log('Server  Socket disconnected');
+    io.emit('notice', {
+      msg: `${nickNameList[socket.id]}님이 퇴장하셨습니다.`,
+    });
+    // delete nickNameList[socket.id];
   });
 });
 
--- 230208/src/App.js
+++ 230208/src/App.js
@@ -1,5 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 let socket = io.connect('http://localhost:8000');
@@ -9,52 +11,10 @@ socket.on('connect', () => {
   console.log('server connected');
 });
 
-// Realtime User Notice
-socket.on('notice', (data) => {
-  document.getElementById('chatIntro').innerText = data.msg;
-  nickNameList = data.nickNameList;
-});
-
-// MyChat 전송
-const sendChat = () => {
-  socket.emit('sendChat', {
-    user_id: socket.id,
-    msg: document.getElementById('chatInput').value,
-  });
-};
-
-// MyChat과 다른 Chat 구별하여 수신
-socket.on('getChat', (data) => {
-  document
-    .getElementById('chatBox')
-    .insertAdjacentHTML(
-      'beforeend',
-      data.user_id == socket.id
-        ? `<div class='MyChatBox'><div class='MyChat'>${data.msg}</div></div>`
-        : `<div class='ServerChat'>${data.msg}</div>`
-    );
-});
-
-// 보낸 DM과 받은 DM 구별하여 수신
-socket.on('getDM', (data) => {
-  document
-    .getElementById('chatBox')
-    .insertAdjacentHTML(
-      'beforeend',
-      data.from_id == socket.id
-        ? `<div class='MyChatBox'><div class='MyDM'>DM to ${
-            nickNameList[data.to_id]
-          } : ${data.msg}</div></div>`
-        : `<div class='DirectChat'>${
-            nickNameList[data.from_id] || 'Server'
-          } : ${data.msg}</div>`
-    );
-});
-
 // Connected Socket list
 const showNameList = () => {
   return Object.keys(nickNameList).map((e, i) =>
-    e == socket.id ? (
+    e === socket.id || e === 'admin' ? (
       false
     ) : (
       <option key={i} value={e}>
@@ -67,61 +27,133 @@ const showNameList = () => {
 function App() {
   const [direct, setDirect] = useState(false);
 
+  const chatInput = useRef();
+  const DMchatInput = useRef();
   const selectDM = useRef();
   const exitDMBtn = useRef();
+  const chatBox = useRef();
 
   const showDM = () => {
     setDirect(!direct);
     selectDM.current.focus();
   };
 
+  // Realtime User Notice
+  socket.on('notice', (data) => {
+    chatBox.current.insertAdjacentHTML(
+      'beforeend',
+      `<div class='chatIntro'>${data.msg}</div>`
+    );
+    nickNameList = data.nickNameList;
+  });
+
+  // MyChat 전송
+  const sendChat = () => {
+    socket.emit('sendChat', {
+      user_id: socket.id,
+      msg: chatInput.current.value,
+    });
+  };
+
+  const enterSendChat = (e) => {
+    if (e.key === 'Enter') sendChat();
+  };
+
+  // Chat Event 한번만 발생시킴
+  useEffect(() => {
+    // MyChat과 다른 Chat 구별하여 수신
+    socket.on('getChat', (data) => {
+      chatBox.current.insertAdjacentHTML(
+        'beforeend',
+        data.user_id === socket.id
+          ? `<div class='MyChatBox'><div class='MyChat'>${data.msg}</div></div>`
+          : `<div class='ServerChat'>${data.msg}</div>`
+      );
+    });
+
+    // 보낸 DM과 받은 DM 구별하여 수신
+    socket.on('getDM', (data) => {
+      console.log('getDM');
+      console.log('data : ', data);
+      chatBox.current.insertAdjacentHTML(
+        'beforeend',
+        data.from_id === socket.id
+          ? `<div class='MyChatBox'><div class='MyDM'>DM to ${
+              nickNameList[data.to_id]
+            } : ${data.msg}</div></div>`
+          : `<div class='DirectChat'>${nickNameList[data.from_id]} : ${
+              data.msg
+            }</div>`
+      );
+    });
+  }, []);
+
   // DM 보냄
   const sendDM = () => {
     socket.emit('sendDM', {
       from_id: socket.id,
       to_id: selectDM.current.value,
-      msg: document.getElementById('DMchatInput').value,
+      msg: DMchatInput.current.value,
     });
     exitDMBtn.current.focus();
+  };
+
+  const enterSendDM = (e) => {
+    if (e.key === 'Enter') sendDM();
   };
 
   //// UI -------------------------------------///
   return (
     <div id="chatBG">
-      <div id="chatIntro"></div>
-      <div id="chatBox"></div>
+      <div ref={chatBox} id="chatBox"></div>
 
       {/* DM O 입력창 */}
       {direct && (
         <div id="DMinputBox">
-          <button ref={exitDMBtn} onClick={showDM} id="XdirectBtn">
+          <Button
+            variant="light"
+            ref={exitDMBtn}
+            onClick={showDM}
+            id="XdirectBtn"
+          >
             X
-          </button>
+          </Button>
           <select ref={selectDM} id="selectDM">
             {showNameList()}
           </select>
-          <input type="text" id="DMchatInput" />
-          <button onClick={sendDM} id="DMchatInputBtn">
+          <input
+            ref={DMchatInput}
+            type="text"
+            id="DMchatInput"
+            onKeyDown={enterSendDM}
+          />
+          <Button variant="light" onClick={sendDM} id="DMchatInputBtn">
             SEND
-          </button>
+          </Button>
         </div>
       )}
 
       {/* DM X 입력창 */}
       {direct || (
         <div id="inputBox">
-          <button
+          <Button
+            variant="light"
             onClick={() => {
               setDirect(!direct);
             }}
             id="directBtn"
           >
             DM
-          </button>
-          <input type="text" id="chatInput" />
-          <button onClick={sendChat} id="chatInputBtn">
+          </Button>
+          <input
+            ref={chatInput}
+            type="text"
+            id="chatInput"
+            onKeyDown={enterSendChat}
+          />
+          <Button variant="light" onClick={sendChat} id="chatInputBtn">
             SEND
-          </button>
+          </Button>
         </div>
       )}
     </div>
