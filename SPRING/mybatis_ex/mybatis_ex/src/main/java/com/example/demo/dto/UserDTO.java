1792a4ee8459c5f7aa7dfb0f6b2af3576cb73096
nietzche15 <yeongeun93@gmail.com>
2023-03-14 16:46:53 +0900

    230314 SPRING MyBatis Ex

---
+++ SPRING/mybatis_ex/mybatis_ex/src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String id;
+    private int password;
+    private String name;
+    private String nickname;
+    private int no;
+}
